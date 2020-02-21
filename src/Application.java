b96dbe7ad3dc2d8abce4e9b310cb0836795eeac5
vbalan <vbalan@patriot.ambianta.local>
2020-02-21 16:54:35 +0200

    With argument

--- src/Application.java
+++ src/Application.java
@@ -3,14 +3,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		Box firstTestObject = Box.getInstance();
+		Box firstTestObject = Box.getInstance(500);
 		System.out.println(firstTestObject.getValue());
 		
-		firstTestObject.setValue(500);
-		System.out.println(firstTestObject.getValue());
-				
-		Box secondTestObject = Box.getInstance();
-		firstTestObject.setValue(1000);
+		Box secondTestObject = Box.getInstance(1000);
 		System.out.println(secondTestObject.getValue());
 		
 		System.out.println(firstTestObject.toString());
--- src/Box.java
+++ src/Box.java
@@ -3,6 +3,7 @@ public class Box {
 	//1. Внутрь Box могут попасть любые типы данных, т.к. поле value принимает данные типа object.
 	
 	private Object value;
+	private static volatile Box instance;
 	
 	private Box(Object value) {
 		 this.value = value;
@@ -20,10 +21,22 @@ public class Box {
 	
 	//Singelton
 	
-	private static class Holder{
-		private static final Box INSTANCE = new Box(null);
-	}
-	public static Box getInstance() {
-		return Holder.INSTANCE;
+//	private static class Holder{
+//		private static final Box INSTANCE = new Box();
+//	}
+//	public static Box getInstance() {
+//		return Holder.INSTANCE;
+//	}
+	public static Box getInstance(Object value) {
+		Box localInstance = instance;
+		if(localInstance == null) {
+			synchronized (Box.class) {
+				localInstance = instance;
+				if(localInstance == null) {
+					instance = localInstance = new Box(value);
+				}
+			}			
+		}
+		return localInstance;
 	}
 }
